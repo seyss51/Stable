0fde1244441f06dec03d00095ce03bf1cfc06bc7
seyss51 <seyss51@gmail.com>
2026-08-28 16:52:04 +0200

    Create app/src/main/java/com/stable/app/StableApp.kt

---
+++ app/src/main/java/com/stable/app/StableApp.kt
@@ -0,0 +1,9 @@
+package com.stable.app
+
+import androidx.compose.runtime.Composable
+import com.stable.app.ui.screens.CommandCenterScreen
+
+@Composable
+fun StableApp() {
+    CommandCenterScreen()
+}
