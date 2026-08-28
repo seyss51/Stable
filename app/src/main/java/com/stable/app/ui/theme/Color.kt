6336dee5627524e21b414c6c8defaa18af4e807d
seyss51 <seyss51@gmail.com>
2026-08-28 16:53:39 +0200

    Create theme/Color.kt

---
+++ app/src/main/java/com/stable/app/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.stable.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Cockpit
+val StableBlack = Color(0xFF111315)
+val StableAnthracite = Color(0xFF1D1F23)
+val StableSurface = Color(0xFF2A2D33)
+
+// Orange crépuscule
+val StableOrange = Color(0xFFFF7A1A)
+val StableOrangeDark = Color(0xFFE86A00)
+
+// Texte
+val StableWhite = Color(0xFFF4F4F4)
+val StableGrey = Color(0xFFB8B8B8)
+
+// États
+val StableSuccess = Color(0xFF43A047)
+val StableWarning = Color(0xFFFFB300)
+val StableDanger = Color(0xFFE53935)
