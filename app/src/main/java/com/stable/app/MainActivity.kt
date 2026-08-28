fec0819db11d943be22bb7b10ec7b2cfd018ce4d
seyss51 <seyss51@gmail.com>
2026-08-28 16:51:47 +0200

    Create app/src/main/java/com/stable/app/MainActivity.kt

---
+++ app/src/main/java/com/stable/app/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.stable.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.stable.app.ui.theme.StableTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            StableTheme {
+                StableApp()
+            }
+        }
+    }
+}
