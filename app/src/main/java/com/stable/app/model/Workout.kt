f3bfdb3fc7aa98d77db13be731388bf781ad4c86
seyss51 <seyss51@gmail.com>
2026-08-29 01:36:33 +0200

    Create app/src/main/java/com/stable/app/model/Workout.kt

---
+++ app/src/main/java/com/stable/app/model/Workout.kt
@@ -0,0 +1,17 @@
+package com.stable.app.model
+
+data class Workout(
+
+    val id: String,
+
+    val title: String,
+
+    val phase: Int,
+
+    val week: Int,
+
+    val estimatedMinutes: Int,
+
+    val exercises: List<Exercise>
+
+)
