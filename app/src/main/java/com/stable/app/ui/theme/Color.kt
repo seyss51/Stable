b7a24208cbfd9f6406a225e35a8cba77d5d97765
seyss51 <seyss51@gmail.com>
2026-08-29 21:10:54 +0200

    Update Color.kt

--- app/src/main/java/com/stable/app/ui/theme/Color.kt
+++ app/src/main/java/com/stable/app/ui/theme/Color.kt
@@ -2,20 +2,15 @@ package com.stable.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Cockpit
-val StableBlack = Color(0xFF111315)
-val StableAnthracite = Color(0xFF1D1F23)
-val StableSurface = Color(0xFF2A2D33)
+val StableBlack = Color(0xFF101214)
+val StableBackground = Color(0xFF17191C)
+val StableSurface = Color(0xFF23262B)
 
-// Orange crépuscule
 val StableOrange = Color(0xFFFF7A1A)
 val StableOrangeDark = Color(0xFFE86A00)
 
-// Texte
-val StableWhite = Color(0xFFF4F4F4)
-val StableGrey = Color(0xFFB8B8B8)
+val StableWhite = Color(0xFFF3F3F3)
+val StableGrey = Color(0xFFB6B6B6)
 
-// États
-val StableSuccess = Color(0xFF43A047)
-val StableWarning = Color(0xFFFFB300)
-val StableDanger = Color(0xFFE53935)
+val StableGreen = Color(0xFF43A047)
+val StableRed = Color(0xFFE53935)
