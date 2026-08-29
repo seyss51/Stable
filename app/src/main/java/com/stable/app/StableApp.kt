16633e6c1ef937e458fb2229085ed61c24e42fbb
seyss51 <seyss51@gmail.com>
2026-08-29 22:54:19 +0200

    Update StableApp.kt

--- app/src/main/java/com/stable/app/StableApp.kt
+++ app/src/main/java/com/stable/app/StableApp.kt
@@ -1,39 +1,22 @@
 package com.stable.app
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.stable.app.navigation.StableNavHost
+import com.stable.app.ui.theme.STABLETheme
 
 @Composable
 fun StableApp() {
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
+    STABLETheme {
 
-        Scaffold { padding ->
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
 
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(padding),
-                contentAlignment = Alignment.Center
-            ) {
-
-                Text(
-                    text = "S.T.A.B.L.E.\nVersion 0.1.0",
-                    style = MaterialTheme.typography.headlineMedium
-                )
-
-            }
+            StableNavHost()
 
         }
 
