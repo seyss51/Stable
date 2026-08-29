61b3c95c57fdbed321639465c5eb28e6c19ec9a2
seyss51 <seyss51@gmail.com>
2026-08-29 21:15:28 +0200

    Update WorkoutState.kt

--- app/src/main/java/com/stable/app/engine/WorkoutState.kt
+++ app/src/main/java/com/stable/app/engine/WorkoutState.kt
@@ -6,13 +6,17 @@ data class WorkoutState(
 
     val status: WorkoutStatus = WorkoutStatus.IDLE,
 
+    val currentExercise: Exercise? = null,
+
     val exerciseIndex: Int = 0,
 
-    val exercise: Exercise? = null,
+    val totalExercises: Int = 0,
+
+    val countdown: Int = 5,
 
     val remainingSeconds: Int = 0,
 
-    val totalExercises: Int = 0,
+    val restSeconds: Int = 0,
 
     val paused: Boolean = false,
 
