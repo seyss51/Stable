da65fafb881f95b847fa3119f7b4937febf7e794
seyss51 <seyss51@gmail.com>
2026-08-29 20:50:18 +0200

    Update WorkoutState.kt

--- app/src/main/java/com/stable/app/engine/WorkoutState.kt
+++ app/src/main/java/com/stable/app/engine/WorkoutState.kt
@@ -4,13 +4,17 @@ import com.stable.app.model.Exercise
 
 data class WorkoutState(
 
-    val currentExercise: Exercise? = null,
+    val status: WorkoutStatus = WorkoutStatus.IDLE,
 
-    val currentIndex: Int = 0,
+    val exerciseIndex: Int = 0,
 
-    val remainingTime: Int = 0,
+    val exercise: Exercise? = null,
 
-    val running: Boolean = false,
+    val remainingSeconds: Int = 0,
+
+    val totalExercises: Int = 0,
+
+    val paused: Boolean = false,
 
     val finished: Boolean = false
 
