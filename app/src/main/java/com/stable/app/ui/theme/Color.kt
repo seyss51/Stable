4f1f35d3beda1ad1bb618f776a102c9978545b16
seyss51 <seyss51@gmail.com>
2026-08-29 22:38:05 +0200

    Create app/src/main/java/com/stable/app/ui/theme/Color.kt

---
+++ app/src/main/java/com/stable/app/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.stable.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black900 = Color(0xFF090909)
+val Black800 = Color(0xFF151515)
+
+val Orange500 = Color(0xFFFF6F00)
+val Orange300 = Color(0xFFFFA040)
+
+val Gray100 = Color(0xFFE5E5E5)
+val Gray300 = Color(0xFFB0B0B0)
+
+val SurfaceDark = Color(0xFF121212)
