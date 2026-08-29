1079482f36b592d60aa83bb2f43e274cf2ebc03c
seyss51 <seyss51@gmail.com>
2026-08-29 22:33:52 +0200

    Update MainActivity.kt

--- app/src/main/java/com/stable/app/MainActivity.kt
+++ app/src/main/java/com/stable/app/MainActivity.kt
@@ -3,17 +3,19 @@ package com.stable.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.stable.app.navigation.AppNavigation
-import com.stable.app.ui.theme.StableTheme
+import androidx.activity.enableEdgeToEdge
+import com.stable.app.ui.theme.STABLETheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
+
         setContent {
-            StableTheme {
-                AppNavigation()
+            STABLETheme {
+                StableApp()
             }
         }
     }
