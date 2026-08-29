4ed98f60426e8e4dcff3df7908144fa9703c8694
seyss51 <seyss51@gmail.com>
2026-08-29 22:54:55 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ android {
 }
 
 dependencies {
-
+implementation("androidx.navigation:navigation-compose:2.9.3")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
     implementation("androidx.activity:activity-compose:1.10.1")
