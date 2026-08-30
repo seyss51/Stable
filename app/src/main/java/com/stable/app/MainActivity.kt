9afc4e01bc5e7b452bee608041804701461b0e3e
seyss51 <seyss51@gmail.com>
2026-08-30 12:34:33 +0200

    Update MainActivity.kt

--- app/src/main/java/com/stable/app/MainActivity.kt
+++ app/src/main/java/com/stable/app/MainActivity.kt
@@ -4,19 +4,51 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.stable.app.navigation.StableNavHost
 import com.stable.app.ui.theme.STABLETheme
+import com.stable.app.viewmodel.WorkoutViewModel
+import com.stable.app.viewmodel.WorkoutViewModelFactory
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
 
         setContent {
+
+            val repository =
+                (application as StableApplication)
+                    .repository
+
+            val workoutViewModel: WorkoutViewModel =
+                viewModel(
+                    factory = WorkoutViewModelFactory(
+                        repository
+                    )
+                )
+
             STABLETheme {
-                StableApp()
+
+                Surface(
+                    modifier = Modifier
+                ) {
+
+                    StableNavHost(
+                        workoutViewModel = workoutViewModel
+                    )
+
+                }
+
             }
+
         }
+
     }
+
 }
