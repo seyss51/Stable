3fa029698a58dfb0dd31b2195b3908729e87db26
seyss51 <seyss51@gmail.com>
2026-08-30 12:38:54 +0200

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,5 +1,19 @@
-@echo off
-set DIRNAME=%~dp0
-set APP_HOME=%DIRNAME%
-set CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
-"java" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+@ECHO OFF
+
+SET DIRNAME=%~dp0
+IF "%DIRNAME%"=="" SET DIRNAME=.
+
+SET APP_HOME=%DIRNAME%
+
+SET CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+IF DEFINED JAVA_HOME (
+    SET JAVA_EXE=%JAVA_HOME%\bin\java.exe
+) ELSE (
+    SET JAVA_EXE=java.exe
+)
+
+"%JAVA_EXE%" ^
+-classpath "%CLASSPATH%" ^
+org.gradle.wrapper.GradleWrapperMain ^
+%*
