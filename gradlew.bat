80c6e13aaed5bc92824e1c2ff6799e8217a7ce0f
seyss51 <seyss51@gmail.com>
2026-08-30 12:27:02 +0200

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,5 @@
+@echo off
+set DIRNAME=%~dp0
+set APP_HOME=%DIRNAME%
+set CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+"java" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
